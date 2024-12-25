c4e770d557218a027c0710b4b59566cf692242b1
garyguo <garyguo19860502@outlook.com>
2024-12-25 21:57:28 +0800

    Init version for vs project

---
+++ Chapter4_LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter4_LeaveManagementSystem.Web.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
